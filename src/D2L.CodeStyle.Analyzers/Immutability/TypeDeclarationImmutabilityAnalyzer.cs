68231f0bb80b82f32624c0d3919211f8da411e0e
Owen Smith <owen@omsmith.ca>
2020-11-09 16:44:23 -0500

    foo

---
+++ src/D2L.CodeStyle.Analyzers/Immutability/TypeDeclarationImmutabilityAnalyzer.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using D2L.CodeStyle.Analyzers.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace D2L.CodeStyle.Analyzers.Immutability {
+	[DiagnosticAnalyzer( LanguageNames.CSharp )]
+	public sealed class TypeDeclarationImmutabilityAnalyzer : DiagnosticAnalyzer {
+
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+			Diagnostics.MemberIsNotReadOnly,
+			Diagnostics.NonImmutableTypeHeldByImmutable
+		);
+
+		public override void Initialize( AnalysisContext context ) {
+			// context.EnableConcurrentExecution();
+			context.RegisterCompilationStartAction( CompilationStart );
+		}
+
+		public static void CompilationStart(
+			CompilationStartAnalysisContext context
+		) {
+			ImmutabilityContext immutabilityContext = new ImmutabilityContext( Array.Empty<ImmutableTypeInfo>() );
+
+			context.RegisterSymbolAction(
+				ctx => AnalyzeNamedType(
+					ctx,
+					immutabilityContext,
+					(INamedTypeSymbol)ctx.Symbol
+				),
+				SymbolKind.NamedType
+			);
+			
+		}
+
+		private static void AnalyzeNamedType(
+			SymbolAnalysisContext context,
+			ImmutabilityContext immutabilityContext,
+			INamedTypeSymbol analyzedType
+		) {
+			if( !ShouldAnalyze( analyzedType ) ) {
+				return;
+			}
+
+			ImmutableArray<ISymbol> membersToAnalyze = analyzedType.GetMembers().Where( s => !s.IsStatic ).ToImmutableArray();
+			foreach( ISymbol member in membersToAnalyze ) {
+				switch( member.Kind ) {
+					case SymbolKind.Method:
+						// Methods aren't data and don't contribute to mutability
+						continue;
+					case SymbolKind.NamedType:
+						// Type declarations aren't data and don't contribute to mutability
+						continue;
+					case SymbolKind.Property:
+						// Auto-implemented properties matter, but are taken care of via the backing field
+						continue;
+					case SymbolKind.Field:
+						AnalyzeField(
+							context,
+							immutabilityContext,
+							analyzedType,
+							member as IFieldSymbol
+						);
+						break;
+					default:
+						continue;
+				}
+			}
+		}
+
+		private static void AnalyzeField(
+			SymbolAnalysisContext context,
+			ImmutabilityContext immutabilityContext,
+			INamedTypeSymbol analyzedType,
+			IFieldSymbol field
+		) {
+			if( field.IsConst ) {
+				return;
+			}
+
+			ISymbol visibleSymbol = field.AssociatedSymbol ?? field;
+			if( IsAudited( visibleSymbol ) ) {
+				return;
+			}
+
+			if( !field.IsReadOnly ) {
+				context.ReportDiagnostic(
+					Diagnostic.Create(
+						Diagnostics.MemberIsNotReadOnly,
+						location: visibleSymbol.Locations.First(),
+						messageArgs: new object[] { visibleSymbol.Kind, visibleSymbol.Name, field.ContainingType.Name }
+					)
+				);
+				return;
+			}
+
+			ITypeSymbol initializerType = null;
+			ExpressionSyntax initializer = visibleSymbol.Equals( field )
+				? visibleSymbol.GetDeclarationSyntax<VariableDeclaratorSyntax>().Initializer?.Value
+				: visibleSymbol.GetDeclarationSyntax<PropertyDeclarationSyntax>().Initializer?.Value;
+			if( initializer != null ) {
+				SemanticModel model = context.Compilation.GetSemanticModel( initializer.SyntaxTree );
+				TypeInfo initializerTypeInfo = model.GetTypeInfo( initializer );
+				initializerType = initializerTypeInfo.Type;
+			}
+
+			if( !immutabilityContext.IsImmutable(
+				initializerType ?? field.Type,
+				field.Type.Equals( initializerType ) ? ImmutableTypeKind.Total : ImmutableTypeKind.Instance,
+				visibleSymbol.Locations.First(),
+				out Diagnostic heldTypeDiagnostic
+			) ) {
+				context.ReportDiagnostic( heldTypeDiagnostic );
+				return;
+			}
+		}
+
+		private static bool IsAudited( ISymbol symbol ) {
+			if( Attributes.Mutability.Audited.IsDefined( symbol ) ) {
+				return true;
+			}
+
+			if( Attributes.Mutability.Unaudited.IsDefined( symbol ) ) {
+				return true;
+			}
+
+			return false;
+		}
+
+		private static bool ShouldAnalyze(
+			INamedTypeSymbol analyzedType
+		) {
+			if( analyzedType.TypeKind == TypeKind.Interface ) {
+				return false;
+			}
+
+			if( Attributes.Objects.Immutable.IsDefined( analyzedType ) ) {
+				return true;
+			}
+
+			if( Attributes.Objects.ImmutableBaseClass.IsDefined( analyzedType ) ) {
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ tests/D2L.CodeStyle.Analyzers.Test/Specs/TypeDeclarationImmutabilityAnalyzer.cs
@@ -0,0 +1,146 @@
+﻿// analyzer: D2L.CodeStyle.Analyzers.Immutability.TypeDeclarationImmutabilityAnalyzer
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+using D2L.CodeStyle.Annotations;
+using D2L.LP.Extensibility.Activation.Domain;
+
+#region Relevant Types
+
+namespace D2L.CodeStyle.Annotations {
+	public static class Objects {
+		public abstract class ImmutableAttributeBase : Attribute {}
+		public sealed class Immutable : ImmutableAttributeBase { }
+		public sealed class ImmutableBaseClassAttribute : ImmutableAttributeBase { }
+
+		[AttributeUsage( validOn: AttributeTargets.Assembly )]
+		public sealed class ImmutableGenericAttribute : Attribute {
+			public ImmutableGenericAttribute( Type type ) { }
+		}
+	}
+	public static class Mutability {
+		public sealed class AuditedAttribute : Attribute { }
+		public sealed class UnauditedAttribute : Attribute {
+			public UnauditedAttribute( Because why ) { }
+		}
+	}
+	public enum Because {
+		ItHasntBeenLookedAt = 1,
+		ItsSketchy = 2,
+		ItsStickyDataOhNooo = 3,
+		WeNeedToMakeTheAnalyzerConsiderThisSafe = 4,
+		ItsUgly = 5,
+		ItsOnDeathRow = 6
+	}
+}
+
+#endregion
+
+namespace SpecTests {
+
+	using static Immutable = Objects.Immutable;
+	using static ImmutableBaseClass = Objects.ImmutableBaseClassAttribute;
+
+	public sealed class Interfaces {
+
+		public interface RegularInterface { }
+
+		[Immutable]
+		public interface InterfaceMarkedImmutable { }
+
+	}
+
+	public sealed class Classes {
+
+		public sealed class RegularClass {
+
+			private static int m_staticWritableImmutableField = 0;
+
+			private static readonly int m_staticReadOnlyImmutableField = 0;
+
+			private int m_writableImmutableField = 0;
+
+			private readonly int m_readonlyImmutableField = 0;
+
+		}
+
+		[Immutable]
+		public sealed class ClassMarkedImmutableImplementingRegularInterface : Interfaces.RegularInterface { }
+
+		[ImmutableBaseClass]
+		public sealed class ClassMarkedImmutableBaseClassImplementingRegularInterface : Interfaces.RegularInterface { }
+
+		[Immutable]
+		public sealed class ClassMarkedImmutable {
+
+			static int m_staticWritableImmutableField = 0;
+
+			static readonly int m_staticReadOnlyImmutableField = 0;
+
+			int /* MemberIsNotReadOnly(Field, m_writableImmutableField, ClassMarkedImmutable) */ m_writableImmutableField /**/ = 0;
+
+			readonly int m_readonlyImmutableField = 0;
+
+			int AutoImplementedImmutableProperty { get; } = 0;
+
+			int ImplementedImmutableProperty { get { return 0; } }
+
+
+
+			static RegularClass m_staticWritableMutableField = new RegularClass();
+
+			static readonly RegularClass m_staticReadOnlyMutableField = new RegularClass();
+
+			RegularClass /* MemberIsNotReadOnly(Field, m_writableMutableField, ClassMarkedImmutable) */ m_writableMutableField /**/ = new RegularClass();
+
+			readonly RegularClass /* NonImmutableTypeHeldByImmutable(Class, RegularClass, ) */ m_readonlyMutableField /**/ = new RegularClass();
+
+			RegularClass /* NonImmutableTypeHeldByImmutable(Class, RegularClass, ) */ AutoImplementedMutableProperty /**/ { get; } = new RegularClass();
+
+			RegularClass ImplementedMutableProperty { get { return new RegularClass(); } }
+
+
+
+			static object m_staticWritableMutableFieldHeldAsSuper = new RegularClass();
+
+			static readonly object m_staticReadOnlyMutableFieldHeldAsSuper = new RegularClass();
+
+			object /* MemberIsNotReadOnly(Field, m_writableMutableFieldHeldAsSuper, ClassMarkedImmutable) */ m_writableMutableFieldHeldAsSuper /**/ = new RegularClass();
+
+			// Cant write this expectation right now
+			// readonly object /* NonImmutableTypeHeldByImmutable(Class, RegularClass, (or [ImmutableBaseClass])) */ m_readonlyMutableFieldHeldAsSuper /**/ = new RegularClass();
+
+			// Cant write this expectation right now
+			// object /* NonImmutableTypeHeldByImmutable(Class, RegularClass, (or [ImmutableBaseClass])) */ AutoImplementedMutablePropertyHeldAsSuper /**/ { get; } = new RegularClass();
+
+			object ImplementedMutablePropertyHeldAsSuper { get { return new RegularClass(); } }
+
+
+
+			static Interfaces.RegularInterface m_staticWritableMutableFieldImmutableInitializer = new ClassMarkedImmutableImplementingRegularInterface();
+
+			static readonly Interfaces.RegularInterface m_staticReadOnlyMutableFieldImmutableInitializer = new ClassMarkedImmutableImplementingRegularInterface();
+
+			Interfaces.RegularInterface /* MemberIsNotReadOnly(Field, m_writableMutableFieldImmutableInitializer, ClassMarkedImmutable) */ m_writableMutableFieldImmutableInitializer /**/ = new ClassMarkedImmutableImplementingRegularInterface();
+
+			readonly Interfaces.RegularInterface m_readonlyMutableFieldImmutableInitializer = new ClassMarkedImmutableImplementingRegularInterface();
+
+			Interfaces.RegularInterface AutoImplementedMutablePropertyImmutableInitializer { get; } = new ClassMarkedImmutableImplementingRegularInterface();
+
+
+
+			static Interfaces.RegularInterface m_staticWritableMutableFieldImmutableBaseClassInitializer = new ClassMarkedImmutableBaseClassImplementingRegularInterface();
+
+			static readonly Interfaces.RegularInterface m_staticReadOnlyMutableFieldImmutableBaseClassInitializer = new ClassMarkedImmutableBaseClassImplementingRegularInterface();
+
+			Interfaces.RegularInterface /* MemberIsNotReadOnly(Field, m_writableMutableFieldImmutableBaseClassInitializer, ClassMarkedImmutable) */ m_writableMutableFieldImmutableBaseClassInitializer /**/ = new ClassMarkedImmutableBaseClassImplementingRegularInterface();
+
+			readonly Interfaces.RegularInterface m_readonlyMutableFieldImmutableBaseClassInitializer = new ClassMarkedImmutableBaseClassImplementingRegularInterface();
+
+			Interfaces.RegularInterface AutoImplementedMutablePropertyImmutableBaseClassInitializer { get; } = new ClassMarkedImmutableBaseClassImplementingRegularInterface();
+
+		}
+
+	}
+
+}
